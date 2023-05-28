2bfb1c3eee26e94721752f14cb1ebbd96edf36e7
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-05-28 15:06:06 +0900

    // 제한시간내에 풀이 [v] // 문제유형: 출력, 연산 // 문제형태: 문자열 붙여서 출력하기

---
+++ training/day02/02.js
@@ -0,0 +1,20 @@
+// 제한시간내에 풀이 [v]
+// 문제유형: 출력, 연산
+// 문제형태: 문자열 붙여서 출력하기
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input = line.split(' ');
+}).on('close', function () {
+  str1 = input[0];
+  str2 = input[1];
+
+  console.log(`${str1}${str2}`);
+});
